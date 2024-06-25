88bc5282fdd9e5d03b894c9a5b3b8fc422337011
ghsheng1206 <65537641+ghsheng1206@users.noreply.github.com>
2024-06-25 14:28:50 +0800

    Updated new practice mode(practice and exam)

--- Renderer/practicePage.js
+++ Renderer/practicePage.js
@@ -1,16 +1,18 @@
 let currentQuestionIndex = 0;
 let currentQuestions = [];
 let userAnswers = [];
-let practicing = false; // Variable to track whether the user is practicing
-let timerInterval; // Variable for the timer interval
-let fileLoaded = false; // Variable to track if a file has been loaded
+let markedQuestions = [];
+let filteredQuestions = [];
+let practicing = false;
+let examMode = false;
+let timerInterval;
+let fileLoaded = false;
 
-// Load the CSV file and start the quiz
 window.loadCSV = async function () {
     const questionContainer = document.getElementById("questionContainer");
     questionContainer.innerHTML = "";
     const form = document.getElementById('csvForm');
-    form.style.display = 'none'; // Hide the form after loading CSV
+    form.style.display = 'none';
 
     const fileInput = document.getElementById("csvFile");
     const file = fileInput.files[0];
@@ -19,7 +21,6 @@ window.loadCSV = async function () {
         window.location.href = "./practicePage.html";
     }
 
-    // Store the file name in localStorage
     localStorage.setItem('quizFileName', file.name);
     const fileName = file.name.replace(".csv", "");
     document.getElementById("fileNameTitle").innerText = fileName;
@@ -32,8 +33,8 @@ window.loadCSV = async function () {
             skipEmptyLines: true,
             complete: function (results) {
                 currentQuestions = results.data;
-                document.getElementById('questionModeForm').style.display = 'block'; // Show the question mode form
-                fileLoaded = true; // Set fileLoaded to true
+                document.getElementById('questionModeForm').style.display = 'block';
+                fileLoaded = true;
             },
             error: function (error) {
                 console.error("CSV parsing error:", error);
@@ -70,10 +71,12 @@ function toggleTimerOptions() {
 }
 
 function startPractice() {
+    const practiceMode = document.getElementById('practiceModeSelect').value;
+    examMode = practiceMode === 'exam';
+
     const mode = document.getElementById('modeSelect').value;
 
     if (mode === 'all') {
-        currentQuestions = currentQuestions;
         generateQuestionNav(currentQuestions.length);
     } else if (mode === 'random') {
         const randomOption = document.querySelector('input[name="randomOption"]:checked').value;
@@ -106,22 +109,25 @@ function startPractice() {
             alert("Please enter a valid number of minutes.");
             return;
         }
-        localStorage.setItem('timerValue', `${minutes} minutes`); // Store the timer value
+        localStorage.setItem('timerValue', `${minutes} minutes`);
         startTimer(minutes);
     } else {
-        localStorage.setItem('timerValue', 'No Timer'); // Store no timer
+        localStorage.setItem('timerValue', 'No Timer');
     }
 
-    document.getElementById('questionModeForm').style.display = 'none'; // Hide the question mode form
-    document.getElementById('navigationButtons').style.display = 'flex'; // Show the navigation buttons
-    document.getElementById('submitQuizButton').style.display = 'block'; // Show the submit button
+    document.getElementById('questionModeForm').style.display = 'none';
+    document.getElementById('navigationButtons').style.display = 'flex';
+    document.getElementById('prevButton').style.display = 'none';
+    document.getElementById('nextButton').style.display = 'inline-block';
+    document.getElementById('submitQuizButton').style.display = 'block';
 
-    // Save the start time
     localStorage.setItem('startTime', new Date().toISOString());
 
+    currentQuestionIndex = 0;  // Reset to the first question
     displayQuestion(currentQuestionIndex);
     startPracticeMode();
 }
+
 
 function getRandomQuestions(questions, count) {
     const shuffled = shuffleArray(questions);
@@ -178,12 +184,19 @@ function generateQuestionNav(totalQuestions) {
         const button = document.createElement('button');
         button.className = 'nav-btn btn';
         button.innerText = i + 1;
-        button.onclick = () => {
-            updateUserAnswer();
-            currentQuestionIndex = i;
-            displayQuestion(currentQuestionIndex);
-            updateQuestionNav();
-        };
+
+        // Disable button click in exam mode
+        if (examMode) {
+            button.disabled = true;
+        } else {
+            button.onclick = () => {
+                updateUserAnswer();
+                currentQuestionIndex = i;
+                displayQuestion(currentQuestionIndex);
+                updateQuestionNav();
+            };
+        }
+
         questionNav.appendChild(button);
     }
     updateQuestionNav();
@@ -194,17 +207,23 @@ function updateQuestionNav() {
     const buttons = questionNav.children;
     for (let i = 0; i < buttons.length; i++) {
         const button = buttons[i];
-        if (i === currentQuestionIndex) {
+        const questionIndex = parseInt(button.innerText) - 1; // Get the actual question index from the button text
+        if (questionIndex === currentQuestionIndex) {
             button.classList.add('active');
         } else {
             button.classList.remove('active');
         }
-        if (userAnswers[i]) {
+        if (userAnswers[questionIndex]) {
             button.classList.add('answered');
             button.classList.remove('incomplete');
         } else {
             button.classList.add('incomplete');
             button.classList.remove('answered');
+        }
+        if (markedQuestions.includes(questionIndex)) {
+            button.style.backgroundColor = 'orange';
+        } else {
+            button.style.backgroundColor = ''; // Reset the color if not marked
         }
     }
 }
@@ -213,11 +232,26 @@ function displayQuestion(index) {
     const questionContainer = document.getElementById("questionContainer");
     questionContainer.innerHTML = "";
     const question = currentQuestions[index];
-    const questionElement = document.createElement('div');
     const questionType = question['Question Type'] || question['Question type'];
-    questionElement.innerHTML = `<h5>Question ${index + 1} of ${currentQuestions.length}</h5>
-                                 <p><strong>Question Type:</strong> ${questionType}</p>
-                                 <p>${question['Question']}</p>`;
+    const isMarked = markedQuestions.includes(index);
+
+    let markIcon = '';
+    if (!examMode) {
+        markIcon = isMarked
+            ? '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-bookmark-check-fill" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5m8.854-9.646a.5.5 0 0 0-.708-.708L7.5 7.793 6.354 6.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0z"/></svg>'
+            : '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-bookmark" viewBox="0 0 16 16"><path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z"/></svg>';
+    }
+
+    const questionElement = document.createElement('div');
+    questionElement.innerHTML = `
+        <h5>Question ${index + 1} of ${currentQuestions.length}</h5>
+        <p><strong>Question Type:</strong> ${questionType}</p>
+        <div style="display: flex; justify-content: space-between; align-items: center;">
+            <p>${question['Question']}</p>
+            ${!examMode ? `<button class="mark-btn" onclick="markQuestion(${index})">${markIcon}</button>` : ''}
+        </div>
+    `;
+
     const storedAnswer = userAnswers[index] || '';
     const storedAnswerArray = storedAnswer.split('');
 
@@ -230,16 +264,36 @@ function displayQuestion(index) {
 
     questionContainer.appendChild(questionElement);
 
-    const correctAnswer = question['Answer'];
-    const isEncrypted = correctAnswer.length === 64 && /^[a-f0-9]{64}$/.test(correctAnswer);
-
     const showAnswerButton = document.getElementById('showAnswerButton');
-    if (isEncrypted) {
-        showAnswerButton.style.display = 'none';
-    } else {
-        showAnswerButton.style.display = 'block';
+    if (showAnswerButton) {
+        if (examMode) {
+            showAnswerButton.style.display = 'none';
+        } else {
+            const correctAnswer = question['Answer'];
+            const isEncrypted = correctAnswer.length === 64 && /^[a-f0-9]{64}$/.test(correctAnswer);
+            showAnswerButton.style.display = isEncrypted ? 'none' : 'block';
+        }
     }
+
+    // Hide the Previous button if in exam mode
+    document.getElementById('prevButton').style.display = examMode ? 'none' : 'inline-block';
 }
+
+
+
+
+function markQuestion(index) {
+    if (markedQuestions.includes(index)) {
+        markedQuestions = markedQuestions.filter(i => i !== index);
+    } else {
+        markedQuestions.push(index);
+    }
+    updateUserAnswer(); // Make sure the current answer is saved before redisplaying
+    updateQuestionNav();
+    displayQuestion(index);  // Update the question display to reflect the new mark status
+}
+
+
 
 function showAnswer() {
     const question = currentQuestions[currentQuestionIndex];
@@ -249,10 +303,9 @@ function showAnswer() {
     if (!isEncrypted) {
         const questionContainer = document.getElementById("questionContainer");
 
-        // Check if the answer has already been shown
         if (!questionContainer.querySelector('.answer-shown')) {
             const answerElement = document.createElement('div');
-            answerElement.className = 'answer-shown'; // Add a class to mark the answer as shown
+            answerElement.className = 'answer-shown';
             answerElement.innerHTML = `<p class="text-success">Correct Answer: ${correctAnswer}</p>`;
             questionContainer.appendChild(answerElement);
         }
@@ -260,18 +313,37 @@ function showAnswer() {
 }
 
 async function submitQuiz() {
+    updateUserAnswer();
+    const practiceMode = document.getElementById('practiceModeSelect').value;
+
+    if (practiceMode === 'practice') {
+        const unansweredQuestions = [];
+        for (let i = 0; i < currentQuestions.length; i++) {
+            if (!userAnswers[i] || markedQuestions.includes(i)) {
+                unansweredQuestions.push(i);
+            }
+        }
+
+        if (unansweredQuestions.length > 0) {
+            const confirmation = confirm("There are marked or unanswered questions. Do you want to submit the quiz?");
+            if (!confirmation) {
+                generateQuestionNavFiltered(unansweredQuestions);
+                currentQuestionIndex = unansweredQuestions[0];
+                displayQuestion(currentQuestionIndex);
+                return;
+            }
+        }
+    }
+
     const confirmation = confirm("Are you sure you want to submit the quiz?");
     if (!confirmation) {
         return;
     }
 
-    updateUserAnswer();
-
     let score = 0;
     const wrongAnswers = [];
     const totalQuestions = currentQuestions.length;
 
-    // Helper function to normalize answers for comparison
     function normalizeAnswer(answer) {
         return answer.toUpperCase().split('').sort().join('');
     }
@@ -286,7 +358,7 @@ async function submitQuiz() {
 
         let isCorrect = false;
         if (isEncrypted) {
-            const sortedUserAnswer = normalizeAnswer(userAnswer);  // Sort the user answer before encrypting
+            const sortedUserAnswer = normalizeAnswer(userAnswer);
             const encryptedUserAnswer = await window.electronAPI.hashAnswer(sortedUserAnswer);
             console.log(`Encrypted User Answer: ${encryptedUserAnswer}`);
             isCorrect = encryptedUserAnswer === correctAnswer;
@@ -302,7 +374,7 @@ async function submitQuiz() {
             wrongAnswers.push({
                 question: question['Question'],
                 userAnswer: userAnswer,
-                correctAnswer: question['Answer']  // Store the correct answer from the original question
+                correctAnswer: question['Answer']
             });
         }
     }
@@ -314,12 +386,11 @@ async function submitQuiz() {
         completionDate: new Date().toISOString(),
         questionMode: document.getElementById('modeSelect').value,
         timer: localStorage.getItem('timerValue'),
-        timeUsed: calculateTimeUsed()  // Add a function to calculate time used if applicable
+        timeUsed: calculateTimeUsed()
     };
 
     console.log("Quiz Results:", results);
 
-    // Store quiz results and all questions in localStorage
     localStorage.setItem('quizResults', JSON.stringify(results));
     localStorage.setItem('allQuestions', JSON.stringify(currentQuestions));
 
@@ -328,11 +399,10 @@ async function submitQuiz() {
 
 
 function calculateTimeUsed() {
-    // Implement this function to calculate the time used if a timer is set
     const startTime = localStorage.getItem('startTime');
     if (startTime) {
         const endTime = new Date();
-        const timeDiff = (endTime - new Date(startTime)) / 1000; // Convert to seconds
+        const timeDiff = (endTime - new Date(startTime)) / 1000;
         const hours = Math.floor(timeDiff / 3600);
         const minutes = Math.floor((timeDiff % 3600) / 60);
         const seconds = Math.floor(timeDiff % 60);
@@ -357,38 +427,100 @@ function updateUserAnswer() {
     if (questionType === 'Multiple-answer') {
         const selectedOptions = document.querySelectorAll('input[name="options"]:checked');
         const optionLetters = Array.from(selectedOptions).map(option => option.value).sort().join('');
-        userAnswers[currentQuestionIndex] = optionLetters; // Store the selected answers for multiple-answer questions
+        userAnswers[currentQuestionIndex] = optionLetters;
         console.log(`Stored answers for question ${currentQuestionIndex + 1}: ${optionLetters}`);
     } else {
         const selectedOption = document.querySelector('input[name="options"]:checked');
         if (selectedOption) {
             const optionLetter = selectedOption.value;
-            userAnswers[currentQuestionIndex] = optionLetter; // Store the selected answer for single-answer questions
+            userAnswers[currentQuestionIndex] = optionLetter;
             console.log(`Stored answer for question ${currentQuestionIndex + 1}: ${optionLetter}`);
         }
     }
-    updateQuestionNav(); // Update the navigation buttons based on the current answers
+    updateQuestionNav();
 }
 
 function nextQuestion() {
     updateUserAnswer();
-    if (currentQuestionIndex < currentQuestions.length - 1) {
-        currentQuestionIndex++;
-        displayQuestion(currentQuestionIndex);
-        updateQuestionNav();
+
+    const practiceMode = document.getElementById('practiceModeSelect').value;
+    const selectedOption = document.querySelector('input[name="options"]:checked');
+
+    if (examMode && !selectedOption) {
+        alert("You must answer this question before moving to the next question.");
+        return;
+    }
+
+    if (practiceMode === 'practice' && filteredQuestions.length > 0) {
+        const currentIndex = filteredQuestions.indexOf(currentQuestionIndex);
+        if (currentIndex < filteredQuestions.length - 1) {
+            currentQuestionIndex = filteredQuestions[currentIndex + 1];
+            displayQuestion(currentQuestionIndex);
+            updateQuestionNav();
+        } else {
+            alert("You have reached the last question. Please review and submit your quiz.");
+        }
     } else {
-        // Notify the user they have reached the last question
-        alert("You have reached the last question. Please review and submit your quiz.");
+        if (currentQuestionIndex < currentQuestions.length - 1) {
+            currentQuestionIndex++;
+            displayQuestion(currentQuestionIndex);
+            updateQuestionNav();
+        } else {
+            alert("You have reached the last question. Please review and submit your quiz.");
+        }
     }
 }
 
+
 function prevQuestion() {
     updateUserAnswer();
-    if (currentQuestionIndex > 0) {
-        currentQuestionIndex--;
-        displayQuestion(currentQuestionIndex);
-        updateQuestionNav();
+
+    const practiceMode = document.getElementById('practiceModeSelect').value;
+    if (practiceMode === 'practice' && filteredQuestions.length > 0) {
+        const currentIndex = filteredQuestions.indexOf(currentQuestionIndex);
+        if (currentIndex > 0) {
+            currentQuestionIndex = filteredQuestions[currentIndex - 1];
+            displayQuestion(currentQuestionIndex);
+            updateQuestionNav();
+        } else {
+            alert("This is the first question.");
+        }
     } else {
-        alert("This is the first question.");
+        if (currentQuestionIndex > 0) {
+            currentQuestionIndex--;
+            displayQuestion(currentQuestionIndex);
+            updateQuestionNav();
+        } else {
+            alert("This is the first question.");
+        }
     }
+}
+
+function generateQuestionNavFiltered(filtered) {
+    filteredQuestions = filtered;  // Update the global filteredQuestions array
+    const questionNav = document.getElementById('questionNav');
+    questionNav.innerHTML = '';
+    filteredQuestions.forEach(i => {
+        const button = document.createElement('button');
+        button.className = 'nav-btn btn';
+        button.innerText = i + 1;
+        button.onclick = () => {
+            updateUserAnswer();
+            currentQuestionIndex = i;
+            displayQuestion(currentQuestionIndex);
+            updateQuestionNav();
+        };
+        if (userAnswers[i]) {
+            button.classList.add('answered');
+            button.classList.remove('incomplete');
+        } else {
+            button.classList.add('incomplete');
+            button.classList.remove('answered');
+        }
+        if (markedQuestions.includes(i)) {
+            button.style.backgroundColor = 'orange';
+        }
+        questionNav.appendChild(button);
+    });
+    updateQuestionNav();
 }
